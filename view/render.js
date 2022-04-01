96718513485bf075eb9f1f43e0ecd1338228be74
ESS User <User@example.com>
2022-04-01 13:15:19 -0400

    progress

--- view/render.js
+++ view/render.js
@@ -14,11 +14,11 @@ function render() {
     layer = new Konva.Layer() // Create a fresh layer
     stage.add(layer) // Add the new layer to the stage
     addGrid(Konva, layer) // Can be removed/commented when game is complete
-
+    renderChoices()
     renderPlayerHand()
     renderComputerHand()
-    renderDiscardPile()
-    renderDeck()
+    //renderDiscardPile()
+    //renderDeck()
 }
 
 function renderPlayerHand() {
@@ -27,47 +27,75 @@ function renderPlayerHand() {
             x: i*80,
             y: 0,
             image: getRawCardImage(model.playerHand[i]), 
-            draggable: true,
+            draggable: false,
         })
         layer.add(cardImage)
 
         // Add event listeners
-        cardImage.on('dragstart', handleDragCardStart)
-        cardImage.on('dragend', function() {
-            handleDragCardEnd(cardImage, i)
-        })
+        //cardImage.on('dragstart', handleDragCardStart)
+        // cardImage.on('dragend', function() {
+        //     handleDragCardEnd(cardImage, i)
+        // })
     }
 }
 
 function renderComputerHand() {
     for (let i = 0; i < model.compHand.length; i++) {
+        if(i== 0){
+            var image = getRawCardImage(model.playerHand[i])
+        }
+        else{
+            var image = rawCardBackImage
+        }
         let cardImage = new Konva.Image({
             x: i*80,
             y: 500,
-            image: rawCardBackImage
+            image: image
         })
         layer.add(cardImage)
     }
 }
 
-function renderDiscardPile() {
-
-    // We just need to show the top card
-    let topCard = getShowingCard()
-    let cardImage = new Konva.Image({
-        x: 250,
-        y: 250,
-        image: getRawCardImage(topCard)
+function renderChoices(){
+    var hit = new Konva.Text({
+        x: 200,
+        y: 300, 
+        text:'Hit',
+        fontSize: 50,
+        fill: 'red',
     })
-    layer.add(cardImage)
+    layer.add(hit)
+    var stay = new Konva.Text({
+        x: 200,
+        y: 400, 
+        text:'stay',
+        fontSize: 50,
+        fill: 'black',
+    })
+    layer.add(stay)
+    hit.on('click', handleHit)
+    stay.on('click', handleStay)
 }
 
-function renderDeck() {
-    let cardImage = new Konva.Image({
-        x: 350,
-        y: 250,
-        image: rawCardBackImage
-    })
-    cardImage.on('click', handleDeckClick)
-    layer.add(cardImage)
-}
+
+// function renderDiscardPile() {
+
+//     // We just need to show the top card
+//     let topCard = getShowingCard()
+//     let cardImage = new Konva.Image({
+//         x: 250,
+//         y: 250,
+//         image: getRawCardImage(topCard)
+//     })
+//     layer.add(cardImage)
+// }
+
+// function renderDeck() {
+//     let cardImage = new Konva.Image({
+//         x: 350,
+//         y: 250,
+//         image: rawCardBackImage
+//     })
+//     cardImage.on('click', handleDeckClick)
+//     layer.add(cardImage)
+// }
